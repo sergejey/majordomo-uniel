6ba28a7adc2f2779e36ffc1ae4345e084aa52624
Sergei Jeihala <jey@tut.by>
2015-09-22 12:30:40 +0300

    Minor tweak

--- scripts/cycle_uniel.php
+++ scripts/cycle_uniel.php
@@ -35,7 +35,7 @@ while(1)
    // check all 1wire devices
    $unl->updateDevices(); 
   
-   if (file_exists('./reboot') || $_GET['onetime']) 
+   if (file_exists('./reboot') || IsSet($_GET['onetime'])) 
    {
       $db->Disconnect();
       exit;
